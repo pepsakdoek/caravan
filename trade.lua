f08eb707afd206eb430f367bb6ba2ff60b58c012
Pepsakdoek <pepsakdoek@gmail.com>
2026-01-15 23:23:38 +0200

    The trade button didn't work (i was opening it up via :lua ingame, so forcing this inside the file makes it work)

--- trade.lua
+++ trade.lua
@@ -12,6 +12,7 @@ local predicates = reqscript('internal/caravan/predicates')
 local classifier = reqscript('internal/caravan/item_classifier')
 local sorting = reqscript('internal/caravan/sorting')
 local ethics = reqscript('internal/caravan/ethics')
+local tradeoverlay = reqscript('internal/caravan/tradeoverlay')
 local utils = require('utils')
 local widgets = require('gui.widgets')
 
@@ -24,7 +25,7 @@ local trade = df.global.game.main_interface.trade
 Trade = defclass(Trade, widgets.Window)
 Trade.ATTRS {
     frame_title='Select trade goods',
-    frame={w=130, h=47},
+    frame={w=150, h=47},
     resizable=true,
     resize_min={w=48, h=40},
 }
@@ -533,3 +534,41 @@ end
 
 EthicsScreen = ethics.EthicsScreen
 TradeEthicsWarningOverlay = ethics.TradeEthicsWarningOverlay
+
+TradeOverlay = tradeoverlay.TradeOverlay
+
+-- -------------------
+-- TradeBannerOverlay
+--
+
+TradeBannerOverlay = defclass(TradeBannerOverlay, overlay.OverlayWidget)
+TradeBannerOverlay.ATTRS{
+    desc='Adds link to the trade screen to launch the DFHack trade UI.',
+    default_pos={x=-31,y=-7},
+    default_enabled=true,
+    viewscreens='dwarfmode/Trade/Default',
+    frame={w=25, h=1},
+    frame_background=gui.CLEAR_PEN,
+}
+
+function TradeBannerOverlay:init()
+    self:addviews{
+        widgets.TextButton{
+            frame={t=0, l=0},
+            label='DFHack trade UI',
+            key='CUSTOM_CTRL_T',
+            enabled=function() return trade.stillunloading == 0 and trade.havetalker == 1 end,
+            on_activate=function() trade_view = trade_view and trade_view:raise() or TradeScreen{}:show() end,
+        },
+    }
+end
+
+function TradeBannerOverlay:onInput(keys)
+    if TradeBannerOverlay.super.onInput(self, keys) then return true end
+
+    if keys._MOUSE_R or keys.LEAVESCREEN then
+        if trade_view then
+            trade_view:dismiss()
+        end
+    end
+end
